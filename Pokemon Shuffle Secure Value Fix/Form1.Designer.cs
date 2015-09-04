be1385ce4aede52617bcc79e6e66b959a93914bc
BlackDeath <dablackdeath@gmail.com>
2015-09-04 15:21:00 +0200

    .

---
+++ Pokemon Shuffle Secure Value Fix/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Pokemon_Shuffle_Secure_Value_Fix
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.savegamename1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.savegamename2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(330, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Fix Secure Value";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.savegamename1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(162, 78);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "[ corrupt save ]";
+            // 
+            // savegamename1
+            // 
+            this.savegamename1.Location = new System.Drawing.Point(6, 48);
+            this.savegamename1.Name = "savegamename1";
+            this.savegamename1.Size = new System.Drawing.Size(148, 20);
+            this.savegamename1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.savegamename2);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Location = new System.Drawing.Point(180, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(162, 78);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "[ latest \"good\" save ]";
+            // 
+            // savegamename2
+            // 
+            this.savegamename2.Location = new System.Drawing.Point(6, 48);
+            this.savegamename2.Name = "savegamename2";
+            this.savegamename2.Size = new System.Drawing.Size(148, 20);
+            this.savegamename2.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(148, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "open";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 131);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Pokemon Shuffle Secure Value Quick Fix by BlackDeath";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox savegamename1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox savegamename2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Pokemon Shuffle Secure Value Fix/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Pokemon_Shuffle_Secure_Value_Fix
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string savegame
+        {
+            get { return savegamename1.Text; }
+        }
+        public string savegame2
+        {
+            get { return savegamename2.Text; }
+        }
+        
+        private void BLKDTH_get_data_good()
+        {
+            OpenFileDialog openFD = new OpenFileDialog();
+            //openFD.InitialDirectory = "c:\\";
+            if (openFD.ShowDialog() == DialogResult.OK)
+            {
+                savegamename2.Text = openFD.FileName;
+            }
+        }
+        private void BLKDTH_get_data_corrupt()
+        {
+            OpenFileDialog openFD = new OpenFileDialog();
+            //openFD.InitialDirectory = "c:\\";
+            if (openFD.ShowDialog() == DialogResult.OK)
+            {
+                savegamename1.Text = openFD.FileName;
+            }
+        }   
+
+        private void BLKDTH_set_data()
+        {
+            FileStream savegame_fs = new FileStream(savegame2, FileMode.Open);
+            BinaryReader savegame_br = new BinaryReader(savegame_fs);
+            long length = savegame_fs.Length;
+            savegame_br.BaseStream.Position = 0x2C;
+            byte[] goodbytes = savegame_br.ReadBytes(8);
+            
+            System.IO.FileStream update_save_open = null;
+            System.IO.BinaryWriter update_save_write = null;
+            update_save_open = new System.IO.FileStream(savegame, System.IO.FileMode.OpenOrCreate);
+            update_save_write = new System.IO.BinaryWriter(update_save_open);
+            update_save_open.Position = Convert.ToInt64("2C", 16);
+            update_save_write.Write(goodbytes);
+            update_save_open.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BLKDTH_get_data_corrupt();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            BLKDTH_get_data_good();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            BLKDTH_set_data();
+            MessageBox.Show("Save should work now");
+        }
+    }
+}
